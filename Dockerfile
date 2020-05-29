34d6954fde6c2acbb8ac3f9855fd299ee6414e7f
Shankar Krishnakumar <shankar.krishnakumar@kroger.com>
2020-05-29 05:44:01 -0400

    Get proper release url

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@ FROM mcr.microsoft.com/dotnet/core/sdk:3.1
 LABEL "com.github.actions.name"="Auto Release Milestone"
 LABEL "com.github.actions.description"="Creates an Autorelease draft"
 
+RUN apt-get update && apt-get install -y jq
+RUN dotnet tool install -g GitReleaseManager.Tool
+
+ENV PATH /root/.dotnet/tools:$PATH
 COPY "entrypoint.sh" /
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 set -u
 
-echo "::set-output name=release-url::https://example.com"
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+  echo "::debug::The event name was '$GITHUB_EVENT_NAME' "
+  exit 0
+fi
+
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+if [ $event_type != "closed"]; then
+  echo "::debug::The event type was '$event_type'"
+fi
+
+milestone=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
+
+release_url=$(dotnet gitreleasemanager create \
+--milestone $milestone \
+--name $milestone \
+--targetcommitish $GITHUB_SHA \
+--token $repo_token \
+--owner $owner \
+--repository $repository)
+
+if [ $? -ne 0 ]; then
+  echo "::error::Failed to create the release-draft"
+  exit 1
+fi
+
+echo "::set-output name=release-url::$release_url"
 
 exit 0
